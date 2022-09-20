0131a28513ae26e6797fbf5e7339861ee836ded4
braulioportela79 <braulioportela@outlook.com>
2022-09-20 19:01:26 -0300

    commit

--- assets/scripts/api/getTask.js
+++ assets/scripts/api/getTask.js
@@ -1,10 +1,8 @@
 // Base URL da API
 const baseUrl = 'https://ctd-todo-api.herokuapp.com/v1';
 
-
-const qs = e => document.querySelector(e);
 const gi = e => document.getElementById(e);
-const qsa = e => document.querySelectorAll(e);
+
 // Variável Elemento Quadro de Tarefas
 const taskBoard = gi('taskBoard');
 const userName = gi('userLogin');
@@ -28,49 +26,11 @@ export const getTask = (token, taskId) => {
 
 // Objeto JSON Registro Usuário
 let userTaskJson = "";
-// const userName = gi('userLogin');
+
 
 const userTask = data => {
-    // userTaskJson = JSON.stringify(data);
-    // sessionStorage.setItem('userTask', userTaskJson);
-    // const userTaskData = sessionStorage.getItem('userTask');
-    // const userTaskObj = JSON.parse(userTaskData);
-    // console.log(userTaskObj)
+
     userTaskJson = JSON.stringify(data);
     userName.innerText = ' ';
 
 };
-
-// const observer = new MutationObserver(() => {
-//     if (document.contains(taskBoard)) {
-//         const deleteTaskBtn = qsa('#deleteTask')
-//         deleteTaskBtn.forEach(e => {
-            
-//             // console.log(userTaskData)
-//             e.addEventListener('click', () => {
-//                 sessionStorage.setItem('userTask', userTaskJson);
-//                 const userTaskData = sessionStorage.getItem('userTask');
-//                 const userTaskObj = JSON.parse(userTaskData);
-//                 console.log(userTaskObj)
-//                 let task = e.parentElement.parentElement;
-//                 let id = task.getAttribute('task-id');
-//                 console.log(id)
-//                 getTask(token, id)
-
-//                 // if (id === userTaskObj.id) {
-//                 //     console.log('igual')
-//                 // } else {
-//                 //     console.log('nao')
-//                 // }
-//                 // // console.log(taskBoard.removeChild(task));
-//                 // task.parentNode.removeChild(task)
-//                 // getTask()
-//                 // console.log(userTaskObj)
-//                 // console.log('123')
-//             })
-//             observer.disconnect();
-//         })
-//     };
-// });
-
-// observer.observe(taskBoard, { childList: true });
